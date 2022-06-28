bc733d796caa94cc2c53b92bbfe67f959156d378
ice_lfernandes <ice_lfernandes@uolinc.com>
2022-06-27 21:13:30 -0300

    melhoria de crud

--- src/main/kotlin/br/com/pocwebflux/movies/endpoint/MovieEndpoint.kt
+++ src/main/kotlin/br/com/pocwebflux/movies/endpoint/MovieEndpoint.kt
@@ -1,14 +1,13 @@
 package br.com.pocwebflux.movies.endpoint
 
+import br.com.pocwebflux.movies.exception.NotFoundException
 import br.com.pocwebflux.movies.model.Movie
 import br.com.pocwebflux.movies.repository.MovieRepository
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.Duration
 import java.util.*
 
 @RestController
@@ -20,6 +19,26 @@ class MovieEndpoint(val repository: MovieRepository) {
     fun post(@RequestBody movie: Movie): Mono<Movie> {
         movie.id = UUID.randomUUID().toString()
         return repository.save(movie)
+    }
+
+    @GetMapping(produces = ["application/stream+json"])
+    fun get(): Flux<Movie> {
+        return repository.findAll()
+            .delayElements(Duration.ofSeconds(3))
+    }
+
+    @PutMapping
+    fun put(@RequestBody movie: Movie): Mono<Movie> {
+        return repository.save(movie)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String): Mono<Void> {
+        return repository.findById(id)
+            .switchIfEmpty(Mono.error(NotFoundException))
+            .flatMap { movie -> repository.delete(movie) }
+            .then(Mono.empty())
     }
 
 }
